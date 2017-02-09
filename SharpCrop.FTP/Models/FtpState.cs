9fecc55c17cb14d2a3233c4bcb2bb57218e46f77
Tornyi Dénes <tornyi.denes@gmail.com>
2017-02-10 00:48:55 +0100

    Disabled core.ignorecase

---
+++ SharpCrop.FTP/Models/FtpState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace SharpCrop.FTP.Models
+{
+    /// <summary>
+    /// Helper model for FtpUploader.
+    /// </summary>
+    public class FtpState
+    {
+        public FtpState()
+        {
+            OperationComplete = new ManualResetEvent(false);
+        }
+
+        public ManualResetEvent OperationComplete { get; }
+        public FtpWebRequest Request;
+        public MemoryStream Stream;
+        public Exception OperationException;
+    }
+}
